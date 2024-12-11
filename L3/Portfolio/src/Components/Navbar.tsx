472b8b73d2d01cd27d9b16406a2e4768a1a02479
Anup <119606225+anup4747@users.noreply.github.com>
2024-12-11 14:51:49 +0530

    Update Navbar.tsx

--- L3/Portfolio/src/Components/Navbar.tsx
+++ L3/Portfolio/src/Components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20 dark:text-white">
           <div className="flex-shrink-0 text-2xl font-bold ml-7 ">
-            <Link to="/">
+            <Link to="/"    onClick={() => setIsOpen(!isOpen)} > 
               <img
                 src={logo}
                 alt=""
