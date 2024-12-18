512ac3fc157a286a83570600267830b7da816bb9
Anup <119606225+anup4747@users.noreply.github.com>
2024-12-18 09:06:01 +0530

    some changes
    Backend

--- L3/Portfolio/api/server.js
+++ L3/Portfolio/api/server.js
@@ -1,16 +1,34 @@
+// api/index.js
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import run from './db/connection.js';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 import contactRoutes from './routes/contact.js';
 
-const app = express();
-app.use(express.json()); 
+dotenv.config();
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Could not connect to MongoDB', err.message);
+    process.exit(1); // Exit process with failure
+  }
+}
 run();
 
 app.use('/api/contact', contactRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+export default app;
